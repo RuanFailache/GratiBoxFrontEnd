fb6e36c718981e7b12eb41af44e75be187ad47f1
RuanFailache <ruanfailache@outlook.com>
2021-11-22 10:37:09 -0300

    fix - register new plan

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,14 @@ import SignIn from './components/Login/SignIn';
 import SignUp from './components/Login/SignUp';
 import SignatureOptions from './components/Signatures/SignatureOptions';
 import SignaturePlans from './components/Signatures/SignaturePlans';
+import SignatureAddress from './components/Signatures/SignatureAddress';
 
 import UserContext from './contexts/UserContext';
 import PlanContext from './contexts/PlanContext';
+import AddressContext from './contexts/AddressContext';
 
 const App = function () {
   const [userInfo, setUserInfo] = useState(null);
-
-  const userInfoState = useMemo(() => ({
-    userInfo,
-    setUserInfo,
-  }), [userInfo]);
 
   const [plan, setPlan] = useState({
     type: null,
@@ -32,21 +29,43 @@ const App = function () {
     itensDelivery: [],
   });
 
+  const [address, setAddress] = useState({
+    userFullName: null,
+    street: null,
+    number: null,
+    complement: null,
+    city: null,
+    state: null,
+    zipCode: null,
+  });
+
+  const userInfoState = useMemo(() => ({
+    userInfo,
+    setUserInfo,
+  }), [userInfo]);
+
   const planState = useMemo(() => ({
     plan,
     setPlan,
+  }), [plan]);
+
+  const addressState = useMemo(() => ({
+    address,
+    setAddress,
   }), [plan]);
 
   return (
     <BrowserRouter>
       <UserContext.Provider value={userInfoState}>
         <PlanContext.Provider value={planState}>
+          <AddressContext.Provider value={addressState} />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="sign-in" element={<SignIn />} />
             <Route path="sign-up" element={<SignUp />} />
             <Route path="signature" element={<SignaturePlans />} />
             <Route path="signature/options" element={<SignatureOptions />} />
+            <Route path="signature/address" element={<SignatureAddress />} />
           </Routes>
         </PlanContext.Provider>
       </UserContext.Provider>
